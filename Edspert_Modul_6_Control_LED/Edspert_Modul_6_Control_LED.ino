a41d95bc272121231a2653d7f41737d591fe5f5c
Fariz Alemuda <farizalemuda@gmail.com>
2024-01-04 21:36:23 +0700

    Update Edspert_Modul_6_Control_LED.ino

--- Edspert_Modul_6_Control_LED/Edspert_Modul_6_Control_LED.ino
+++ Edspert_Modul_6_Control_LED/Edspert_Modul_6_Control_LED.ino
@@ -1,103 +1,67 @@
-// proses include library 
-#include "DHT.h"
-#include "WiFi.h"
-#include "HTTPClient.h"
+#include <ArduinoJson.h>
+#include <StreamUtils.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// deklarasi variable
-// set pin yang digunakan
-#define DHTPIN 15
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define pinLED 23
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 const String appName = "antares-application-name";
 const String devName = "antares-device-name";
 const String accessKey = "antares-access-key";
-
-// Inisiasi sensor DHT
-DHT dht(DHTPIN, DHTTYPE);
-
-// url endpoint data of device
-const String url = String("http://platform.antares.id:8080/~/antares-cse/antares-id/"+appName+"/"+devName);
-
-// inisiasi variable 
-unsigned long lastTime = 0;
-// Setting timer 5 detik
-unsigned long timerDelay = 5000;
+const String url = String("http://platform.antares.id:8080/~/antares-cse/antares-id/"+ appName+"/"+devName+"/la");
 
 void setup() {
-  // inisiasi interface serial
   Serial.begin(115200);
-
-  Serial.println("EDSPERT - Akuisisi sensor DHT22 via ESP32");
-
-  // inisiasi WiFi Client
   WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+
+  pinMode(pinLED, OUTPUT);
 }
 
 void loop() {
-  //rutin melakukan pengiriman data setiap waktu yang diset
-  if ((millis() - lastTime) > timerDelay) {
-    // deklarasi objek untuk menampung data 
-    // temperatur dan kelembapan dari DHT22
-
-    // ekstrak data temperature
-    float temp = dht.readTemperature();
-    // ekstrak data humidity
-    float hum = dht.readHumidity();
-
-    // menampilkan data di serial
-    Serial.println("Suhu: " + String(temp, 2) + "°C");
-    Serial.println("Kelembaban: " + String(hum, 1) + "%");
-    Serial.println("---");
-
-    // waktu jeda sampling data
-    // minimal 2 detik
-    delay(5000);
-
-    // mengecek koneksi wifi
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-    
-      // inisiasi komunikasi http
-      http.begin(client, url);
-
-      // inisiasi http header
-      http.addHeader("X-M2M-Origin", accessKey);
-      http.addHeader("Content-Type", "application/json;ty=4");
-      http.addHeader("Accept", "application/json");
-      
-      // inisiasi data yang dikirim di restful api
-      String httpRequestData ="{\"m2m:cin\": { \"con\": \"{\\\"temp\\\":";
-      httpRequestData = httpRequestData + String(temp, 2);
-      httpRequestData = httpRequestData + ",\\\"hum\\\":";
-      httpRequestData = httpRequestData + String(hum, 1);
-      httpRequestData = httpRequestData + "}\"}}";
-      
-      // mengirimkan HTTP POST request
-      Serial.println(httpRequestData);
-      int httpResponseCode = http.POST(httpRequestData);
-     
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-        
-      // menutup koneksi
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+
+  Serial.print("OK! IP=");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Fetching " + url + "... ");
+
+  HTTPClient http;
+  http.begin(url);
+  http.addHeader("X-M2M-Origin", accessKey);
+  http.addHeader("Content-Type", "application/json;ty=4");
+  http.addHeader("Accept", "application/json");
+  
+  http.GET();
+  
+  DynamicJsonDocument doc(1024);
+  DynamicJsonDocument content(1024);
+
+  ReadLoggingStream loggingStream(http.getStream(), Serial);
+  deserializeJson(doc, loggingStream);
+
+  String con = doc["m2m:cin"]["con"];
+  deserializeJson(content, con);
+  
+  int status = content["status"];
+  Serial.println();
+  Serial.println();
+  Serial.print("status led dari antares: ");
+  Serial.println(status);  
+  
+  if(status == 1){
+    Serial.println("status 1");
+    digitalWrite(pinLED, HIGH);
+  } else {
+    Serial.println("status 0");
+    digitalWrite(pinLED, LOW);
+  }
+  
+  Serial.println();
+  delay(5000);
 }
